509e14aaba2a5f55f7722c23e9dc24d9ad868efd
Jessica Campos <jessica.campos@live.com>
2018-01-26 20:57:52 -0200

    Inclusão de dados que serão inseridos pelo Seed

--- MeuConsultorio/Migrations/Configuration.cs
+++ MeuConsultorio/Migrations/Configuration.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using MeuConsultorio.Models;
+
 namespace MeuConsultorio.Migrations
 {
     using System;
@@ -14,18 +17,65 @@ namespace MeuConsultorio.Migrations
 
         protected override void Seed(MeuConsultorio.Models.MeuConsultorioContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            if (!context.TiposAtendimentos.Any())
+            {
+                List<TipoAtendimento> tipos = new List<TipoAtendimento>()
+                {
+                    new TipoAtendimento(){TipoAtendimentoId = 1, Codigo = 1,Descricao = "Consulta"},
+                    new TipoAtendimento(){TipoAtendimentoId = 2, Codigo = 2,Descricao = "Curativo"},
+                    new TipoAtendimento(){TipoAtendimentoId = 3, Codigo = 3,Descricao = "Exame"}
+                };
+                context.TiposAtendimentos.AddRange(tipos);
+                context.SaveChanges();
+            }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+
+            if (!context.Convenios.Any())
+            {
+                List<Convenio> convenios = new List<Convenio>()
+                {
+                    new Convenio() {ConvenioId = 1, Codigo = 1, Descricao = "Particular", Fatura = false},
+                    new Convenio() {ConvenioId = 2, Codigo = 2, Descricao = "Unimed", Fatura = true},
+                    new Convenio() {ConvenioId = 3, Codigo = 3, Descricao = "Saúde Vida", Fatura = true}
+                };
+
+                context.Convenios.AddRange(convenios);
+                context.SaveChanges();
+            }
+
+            if (!context.Procedimentos.Any())
+            {
+                List<Procedimento> procedimentos = new List<Procedimento>()
+                {
+                    new Procedimento(){ProcedimentoId = 1, Codigo = 1,Descricao = "Consulta médica",TipoAtendimentoId = 1},
+                    new Procedimento(){ProcedimentoId = 2,Codigo = 2,Descricao = "Consulta de aconselhamento para planejamento familiar",TipoAtendimentoId = 1},
+                    new Procedimento(){ProcedimentoId = 3,Codigo = 3,Descricao = "Consulta individual",TipoAtendimentoId = 1},
+                    new Procedimento(){ProcedimentoId = 4,Codigo = 4,Descricao = "Curativos em geral c/ ou s/ anestesia",TipoAtendimentoId = 2},
+                    new Procedimento(){ProcedimentoId = 5,Codigo = 5,Descricao = "Curativo de queimadura",TipoAtendimentoId = 2},
+                    new Procedimento(){ProcedimentoId = 6,Codigo = 6,Descricao = "Curativo especial sob anestesia",TipoAtendimentoId = 2},
+                    new Procedimento(){ProcedimentoId = 7,Codigo = 7,Descricao = "Endoscopia",TipoAtendimentoId = 3},
+                    new Procedimento(){ProcedimentoId = 8,Codigo = 8,Descricao = "Mamografia",TipoAtendimentoId = 3}
+                };
+                context.Procedimentos.AddRange(procedimentos);
+                context.SaveChanges();
+            }
+
+            if (!context.Procedimentos.Any())
+            {
+                List<ProcedimentoConvenio> procedimentos_convenios = new List<ProcedimentoConvenio>()
+                {
+                    new ProcedimentoConvenio(){ProcedimentoConvenioId = 1, Valor = 40, Codigo = 1, ConvenioId = 1, ProcedimentoId = 1},
+                    new ProcedimentoConvenio(){ProcedimentoConvenioId = 2, Valor = 25, Codigo = 2, ConvenioId = 1, ProcedimentoId = 6},
+                    new ProcedimentoConvenio(){ProcedimentoConvenioId = 3, Valor = 200, Codigo = 3, ConvenioId = 3, ProcedimentoId = 1},
+                    new ProcedimentoConvenio(){ProcedimentoConvenioId = 4, Valor = 41, Codigo = 4, ConvenioId = 2, ProcedimentoId = 1},
+                    new ProcedimentoConvenio(){ProcedimentoConvenioId = 5, Valor = 88, Codigo = 5, ConvenioId = 2, ProcedimentoId = 7},
+                    new ProcedimentoConvenio(){ProcedimentoConvenioId = 6, Valor = 75, Codigo = 6, ConvenioId = 1, ProcedimentoId = 7},
+                    new ProcedimentoConvenio(){ProcedimentoConvenioId = 7, Valor = 600, Codigo = 7, ConvenioId = 3, ProcedimentoId = 7}
+                };
+                context.ProcedimentosConvenios.AddRange(procedimentos_convenios);
+                context.SaveChanges();
+            }
+ 
         }
     }
 }
